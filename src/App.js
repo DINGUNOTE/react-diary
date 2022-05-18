32310497b86b6b11fdf4b7df172fdda91b667db1
web.ljg <web.ljg@gmail.com>
2022-05-18 17:05:10 +0900

    # 더미 데이터와 새로 생성된 일기 key 값 겹치는 문제 수정, 매달 마지막 날 일기 표시 안되는 문제 수정

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ const dummyList = [
 function App() {
   const [data, dispatch] = useReducer(reducer, dummyList);
 
-  const dataId = useRef(0);
+  const dataId = useRef(6);
 
   // CREATE
   const onCreate = (date, content, emotion) => {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -25,6 +25,9 @@ const Home = () => {
         curDate.getFullYear(),
         curDate.getMonth() + 1,
         0,
+        23,
+        59,
+        59,
       ).getTime();
       setData(
         diaryList.filter(it => firstDay <= it.date && it.date <= lastDay),
